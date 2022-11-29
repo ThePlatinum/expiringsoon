daf82c914778e4f40807c8157b9db54b07e85392
Platinum <platinumemirate@gmail.com>
2022-11-29 20:58:53 +0100

    setting up

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Container, InputField } from './style/global';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Container>
+
+      <Text>Heyy</Text>
+      <InputField />
       <StatusBar style="auto" />
-    </View>
+    </Container>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ style/global.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+`;
+
+export const InputField = styled.TextInput`
+  border: 2px #00B20730 solid;
+  border-radius: 5px;
+  width: 100px;
+  padding: 5px;
+`;
